645ac6dc27383c22a22d1dec5919e9c6b61bbba7
Timothée <timothee.laurent@yahoo.fr>
2019-01-26 15:06:30 +0100

    Added lungs to oxygen bar

--- code/local/ResourcesViewer.cc
+++ code/local/ResourcesViewer.cc
@@ -3,23 +3,30 @@
 #include <gf/Log.h>
 #include <gf/RenderTarget.h>
 #include <gf/Shapes.h>
+#include <gf/Sprite.h>
 
 #include "Singletons.h"
 
 namespace home {
   static constexpr float OxygenLoss = 4.0f;
-  static constexpr gf::Vector2f OxygenSize = {102.0f, 10.0f};
-  static constexpr gf::Vector2f OxygenPosition = {50.0f, 50.0f};
   ResourcesViewer::ResourcesViewer()
-  : m_oxygen(100.0f) {
+  : m_oxygen(100.0f)
+  , m_oxygenIcon(gResourceManager().getTexture("images/lungs.png")) {
   }
 
   void ResourcesViewer::render(gf::RenderTarget& target, const gf::RenderStates& states) {
-    gf::RectangleShape oxygenBackground, oxygen;
-    /*
-    player.setPosition(m_position);
-    */
+    static constexpr gf::Vector2f OxygenSize = {102.0f, 10.0f};
+    static constexpr gf::Vector2f OxygenPosition = {50.0f, 50.0f};
+    static constexpr float scale = 0.05f;
+    gf::Sprite oxygenIcon;
+    oxygenIcon.setTexture(m_oxygenIcon);
+    oxygenIcon.setScale(scale);
+    oxygenIcon.setAnchor(gf::Anchor::Center);
+    oxygenIcon.setPosition({OxygenPosition.x - 20.0f, OxygenPosition.y + 4.0f});
+    oxygenIcon.setColor({1.0f - m_oxygen / 100, 0.0f, m_oxygen / 100, 1.0f});
+    target.draw(oxygenIcon, states);
 
+    gf::RectangleShape oxygenBackground, oxygen;
     oxygenBackground.setColor(gf::Color::Black);
     oxygenBackground.setAnchor(gf::Anchor::TopLeft);
     oxygenBackground.setSize(OxygenSize);
