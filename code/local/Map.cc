87fe3792da36c91aefd78e52d94b6debb1834225
Julien Bernard <julien.bernard@univ-fcomte.fr>
2019-01-26 11:08:02 +0100

    we have a map!

--- code/local/Map.cc
+++ code/local/Map.cc
@@ -3,27 +3,105 @@
 #include <gf/Log.h>
 #include <gf/RenderTarget.h>
 #include <gf/Shapes.h>
+#include <gf/Tmx.h>
 
 #include "Singletons.h"
 
 namespace home {
-  Map::Map() {
+
+  namespace {
+
+    gf::Vector2u getMapSize(const gf::Path& path) {
+      gf::Path filename = gResourceManager().getAbsolutePath(path);
+      gf::TmxLayers layers;
+
+      bool present = layers.loadFromFile(filename);
+      assert(present);
+
+      return layers.mapSize;
+    }
+
+    class LayersMaker : public gf::TmxVisitor {
+    public:
+      LayersMaker(gf::TileLayer& layer)
+      : m_layer(layer)
+      {
+
+      }
+
+      virtual void visitTileLayer(const gf::TmxLayers& map, const gf::TmxTileLayer& layer) override {
+        if (!layer.visible) {
+          return;
+        }
+
+        assert(map.orientation == gf::TmxOrientation::Staggered);
+        assert(m_layer.getMapSize() == map.mapSize);
+
+        gf::Log::info("Parsing layer '%s'\n", layer.name.c_str());
+
+        m_layer.setBlockSize(map.tileSize);
+
+        unsigned k = 0;
+
+        for (auto& cell : layer.cells) {
+          unsigned i = k % map.mapSize.width;
+          unsigned j = k / map.mapSize.width;
+          assert(j < map.mapSize.height);
+
+          unsigned gid = cell.gid;
+
+          if (gid != 0) {
+            auto tileset = map.getTileSetFromGID(gid);
+            assert(tileset);
+            m_layer.setTileSize(tileset->tileSize);
+
+            gid = gid - tileset->firstGid;
+            m_layer.setTile({ i, j }, gid, cell.flip);
+
+            if (!m_layer.hasTexture()) {
+              assert(tileset->image);
+              const gf::Texture& texture = gResourceManager().getTexture(tileset->image->source);
+              m_layer.setTexture(texture);
+            } else {
+              assert(&gResourceManager().getTexture(tileset->image->source) == &m_layer.getTexture());
+            }
+
+          }
+
+          k++;
+        }
+      }
+
+    private:
+      gf::TileLayer& m_layer;
+    };
+
+    constexpr const char *MapName = "map/MapForDev.tmx";
+  }
+
+  Map::Map()
+  : m_layer(getMapSize(MapName), gf::TileLayer::Staggered)
+  {
+    gf::Path filename = gResourceManager().getAbsolutePath(MapName);
+    gf::TmxLayers layers;
+
+    bool present = layers.loadFromFile(filename);
+    assert(present);
+
+    LayersMaker maker(m_layer);
+    layers.visitLayers(maker);
+
     gMessageManager().registerHandler<CursorMovedPosition>(&Map::onCursorMovedPosition, this);
   }
 
   void Map::render(gf::RenderTarget& target, const gf::RenderStates& states) {
-    gf::RectangleShape rect;
-    rect.setColor(gf::Color::Red);
-    rect.setSize({50.0f, 50.0f});
-    rect.setPosition({10.0f, 10.0f});
-    rect.setAnchor(gf::Anchor::Center);
-    target.draw(rect, states);
+    target.draw(m_layer, states);
   }
 
   gf::MessageStatus Map::onCursorMovedPosition(gf::Id id, gf::Message *msg) {
     assert(id == CursorMovedPosition::type);
     CursorMovedPosition *message = static_cast<CursorMovedPosition*>(msg);
-    //"Mouse coordinates: %f, %f\n", message->position.x, message->position.y);
+//     gf::Log::debug("Mouse coordinates: %f, %f\n", message->position.x, message->position.y);
     return gf::MessageStatus::Keep;
-  }   
+  }
 }
--- code/local/Map.h
+++ code/local/Map.h
@@ -2,14 +2,22 @@
 #define HOME_MAP_H
 
 #include <gf/Entity.h>
+#include <gf/TileLayer.h>
+
 #include "Messages.h"
 
 namespace home {
   class Map : public gf::Entity {
-    public :
-      Map();
-      virtual void render(gf::RenderTarget& target, const gf::RenderStates& states) override;
-      gf::MessageStatus onCursorMovedPosition(gf::Id id, gf::Message *msg);
+  public :
+    Map();
+
+    virtual void render(gf::RenderTarget& target, const gf::RenderStates& states) override;
+
+  private:
+    gf::MessageStatus onCursorMovedPosition(gf::Id id, gf::Message *msg);
+
+  private:
+    gf::TileLayer m_layer;
   };
 }
 
