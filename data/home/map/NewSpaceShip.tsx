01eacaf347da6699d18a3878eb7b9c471d36fa84
Ben Goodwin <goodb25@gmail.com>
2019-01-27 00:12:23 +0100

    Added Invisible Sea, and some Ships ... ;)

---
+++ data/home/map/NewSpaceShip.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="NewSpaceShip" tilewidth="512" tileheight="320" tilecount="1" columns="1">
+ <image source="rawTiles/new_vaisseau/new_vaisseau.png" width="512" height="320"/>
+</tileset>
